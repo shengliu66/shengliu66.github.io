c8be181f69ee2b0f748a7d007c5ab9782fe6ade2
shengliu66 <shengliu@nyu.edu>
2025-02-24 20:14:25 -0800

    Deploy updated static website

---
+++ out/static/chunks/src_app_page_tsx_36054f._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_36054f._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_36054f._.js",
+  "chunks": [
+    "static/chunks/src_app_page_tsx_32efbb._.js",
+    "static/chunks/node_modules_f7d8f5._.js"
+  ],
+  "source": "dynamic"
+});
